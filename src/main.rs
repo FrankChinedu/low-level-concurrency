61bc5dbdf5cf86f3665ce204c8de4732cd10e5dc
Obi Chinedu Frank <frankieetchy@gmail.com>
2024-05-06 05:25:48 +0100

    llc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::thread;
 fn main() {
-    println!("Hello, world!");
+    let numbers = Vec::from_iter(0..=1000);
+    let t = thread::spawn(move || {
+        let len = numbers.len();
+        let sum = numbers.into_iter().sum::<usize>();
+        sum / len
+    });
+    let name = thread::current();
+    let name = name.name().unwrap();
+    let average = t.join().unwrap();
+
+    println!("average: {}, name: {:?}", average, name)
 }
+// fn f() {
+//     println!("Hello from another thread!");
+//     let id = thread::current().id();
+//     println!("This is my thread id: {id:?}");
+// }
