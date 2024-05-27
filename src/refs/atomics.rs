c300cec783f54d6285e84ec546e23981df708abb
Obi Chinedu Frank <frankieetchy@gmail.com>
2024-05-27 07:36:11 +0100

    learning

--- src/refs/atomics.rs
+++ src/refs/atomics.rs
@@ -1,25 +1,32 @@
-use std::sync::atomic::AtomicBool;
+use std::sync::atomic::AtomicUsize;
 use std::sync::atomic::Ordering::Relaxed;
 use std::thread;
+use std::time::Duration;
 
 pub fn run() {
-    static STOP: AtomicBool = AtomicBool::new(false);
-    let background_thread = thread::spawn(|| {
-        while !STOP.load(Relaxed) {
-            some_work();
+    let num_done = &AtomicUsize::new(0);
+    thread::scope(|s| {
+        for t in 0..4 {
+            s.spawn(move || {
+                for i in 0..25 {
+                    println!("T {t}");
+                    process_item(t * 25 + i); // Assuming this takes some time.
+                    num_done.fetch_add(1, Relaxed);
+                }
+            });
         }
+        loop {
+            let n = num_done.load(Relaxed);
+            if n == 100 {
+                break;
+            }
+            println!("Working.. {n}/100 done");
+            thread::sleep(Duration::from_secs(1));
+        }
+        println!("Done!");
     });
-
-    for line in std::io::stdin().lines() {
-        match line.unwrap().as_str() {
-            "help" => println!("commands: help, stop"),
-            "stop" => break,
-            cmd => println!("unknown command: {cmd:?}"),
-        }
-    }
-
-    STOP.store(true, Relaxed);
-    background_thread.join().unwrap();
 }
 
-fn some_work() {}
+fn process_item(t: i32) {
+    println!("PRINTING {}", t)
+}
